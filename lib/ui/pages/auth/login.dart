b4b0ff3ecbe3686d13e0b637a56e9c6f923b3498
LeoLizc <ldlizcano@uninorte.edu.co>
2023-11-07 21:59:13 -0500

    Add login form with background images and validation

--- lib/ui/pages/auth/login.dart
+++ lib/ui/pages/auth/login.dart
@@ -1,37 +1,169 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({Key? key}) : super(key: key);
+  LoginPage({Key? key}) : super(key: key);
+
+  final _formKey = GlobalKey<FormState>();
+  final _userController = TextEditingController();
+  final _passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const TextField(
-              decoration: InputDecoration(
-                labelText: 'Email',
+      body: Stack(
+        children: [
+          // Fondo (Capa 1)
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(
+                    'assets/images/LogBackground.png'), // Ruta de tu imagen de fondo
+                fit: BoxFit.cover,
               ),
             ),
-            const SizedBox(height: 16),
-            const TextField(
-              obscureText: true,
-              decoration: InputDecoration(
-                labelText: 'Password',
+          ),
+          // Montañas (Capa 2)
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Image.asset(
+              'assets/images/Background Hills.png', // Ruta de tu segunda imagen
+              fit: BoxFit.cover, // Ancho de la imagen
+            ),
+          ),
+          // Contenido Principal (Capa 3)
+          SafeArea(
+            child: Container(
+              //colocar un margen
+              margin: const EdgeInsets.all(25),
+              //vamos a hacer un login
+
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    SvgPicture.asset('assets/icons/Logo.svg'),
+                    const Text(
+                      'Sign In',
+                      style: TextStyle(fontSize: 30, color: Colors.white),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        const Align(
+                          alignment: Alignment.topLeft,
+                          child: Text(
+                            'Usuario',
+                            style: TextStyle(fontSize: 20, color: Colors.white),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        TextFormField(
+                          controller: _userController,
+                          decoration: const InputDecoration(
+                            //colocar un texto blanco
+                            hintText: 'PepitoPerez24',
+                            hintStyle: TextStyle(
+                                color: Color.fromARGB(125, 255, 255, 255)),
+                            border: OutlineInputBorder(),
+                            enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.white),
+                            ),
+                          ),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Por favor ingrese un usuario';
+                            }
+                            return null;
+                          },
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        const Align(
+                          alignment: Alignment.topLeft,
+                          child: Text(
+                            'Contraseña',
+                            style: TextStyle(fontSize: 20, color: Colors.white),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        TextFormField(
+                          controller: _passwordController,
+                          obscureText: true,
+                          decoration: const InputDecoration(
+                            hintText: '*********',
+                            hintStyle: TextStyle(
+                                color: Color.fromARGB(125, 255, 255, 255)),
+                            border: OutlineInputBorder(),
+                            enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.white),
+                            ),
+                          ),
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Por favor ingrese una contraseña';
+                            }
+                            return null;
+                          },
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    const Text(
+                      'Olvidaste tu contraseña? Registrate',
+                      style: TextStyle(fontSize: 10, color: Colors.white),
+                    ),
+                    const SizedBox(
+                      height: 50,
+                    ),
+                    Row(
+                      children: [
+                        Expanded(
+                          child: ElevatedButton(
+                            onPressed: () {
+                              if (_formKey.currentState!.validate()) {
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  const SnackBar(
+                                    content: Text('Procesando datos'),
+                                  ),
+                                );
+                              }
+                            },
+                            // style: ElevatedButton.styleFrom(
+                            //   minimumSize: const Size(200, 50),
+                            //   backgroundColor: ThemeData().primaryColorDark,
+                            // ),
+                            child: const Text(
+                              'Login',
+                              // style: TextStyle(fontSize: 20, color: Colors.white),
+                            ),
+                            //aumenta el tamaño del boton
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
-            const SizedBox(height: 32),
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text('Login'),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
