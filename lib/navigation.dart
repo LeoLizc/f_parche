9fb30650fd42bac9d3b7446f2376d3c83af3c30d
LeoLizc <ldlizcano@uninorte.edu.co>
2023-11-13 19:53:10 -0500

    Add navigation routes and pages

---
+++ lib/navigation.dart
@@ -0,0 +1,53 @@
+import 'package:f_parche/ui/middlewares/auth_mw.dart';
+import 'package:f_parche/ui/pages/auth/login.dart';
+import 'package:f_parche/ui/pages/auth/signup.dart';
+import 'package:f_parche/ui/pages/chat.dart';
+import 'package:f_parche/ui/pages/create_parche.dart';
+import 'package:f_parche/ui/pages/home.dart';
+import 'package:f_parche/ui/pages/mapa.dart';
+import 'package:f_parche/ui/pages/parches.dart';
+import 'package:get/get.dart';
+
+abstract class Routes {
+  static const home = '/home';
+  static const login = '/login';
+  static const register = '/register';
+  static const parches = '/parches';
+  static const create = '/create';
+  static const chat = '/chat';
+  static const map = '/map';
+}
+
+final appPages = [
+  GetPage(
+    name: '/',
+    page: () => LoginPage(),
+  ),
+  // GetPage(name: '/', page: () => const HomePage()),
+  GetPage(
+    name: Routes.login,
+    page: () => LoginPage(),
+  ),
+  GetPage(
+    name: Routes.register,
+    page: () => SignUpPage(),
+  ),
+  GetPage(name: Routes.home, page: () => const HomePage(), middlewares: [
+    ProtectedRoute(),
+  ]),
+  GetPage(
+      name: Routes.parches,
+      page: () => const ParchePage()), // TODO Corregir tamaños
+  GetPage(
+    name: Routes.create,
+    page: () => CreateParchePage(),
+  ),
+  GetPage(
+    name: Routes.chat,
+    page: () => const ChatRoomPage(),
+  ),
+  GetPage(
+    name: Routes.map,
+    page: () => const MapaPage(),
+  ),
+];
--- lib/ui/app.dart
+++ lib/ui/app.dart
@@ -1,11 +1,49 @@
-import 'package:f_parche/ui/middlewares/auth_mw.dart';
+import 'package:f_parche/navigation.dart';
 import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-import 'pages/auth/login.dart';
-import 'pages/auth/signup.dart';
-import 'pages/home.dart';
+// TODO: Cambiar de sitio la declaración de los temas
+final lightTheme = FlexThemeData.light(
+  colors: const FlexSchemeColor(
+    primary: Color(0xfffc6411),
+    primaryContainer: Color(0xfffeaa68),
+    secondary: Color(0xff2979ff),
+    secondaryContainer: Color(0xffe4eaff),
+    tertiary: Color(0xff2962ff),
+    tertiaryContainer: Color(0xffcbd6ff),
+    appBarColor: Color(0xffe4eaff),
+    error: Color(0xffb00020),
+  ),
+  surfaceMode: FlexSurfaceMode.levelSurfacesLowScaffold,
+  blendLevel: 7,
+  appBarStyle: FlexAppBarStyle.primary,
+  visualDensity: FlexColorScheme.comfortablePlatformDensity,
+  useMaterial3: true,
+  swapLegacyOnMaterial3: true,
+  // To use the playground font, add GoogleFonts package and uncomment
+  // fontFamily: GoogleFonts.notoSans().fontFamily,
+);
+
+final darkTheme = FlexThemeData.dark(
+  colors: const FlexSchemeColor(
+    primary: Color(0xffffb300),
+    primaryContainer: Color(0xff00325b),
+    secondary: Color(0xff82b1ff),
+    secondaryContainer: Color(0xff3770cf),
+    tertiary: Color(0xff448aff),
+    tertiaryContainer: Color(0xff0b429c),
+    appBarColor: Color(0xff3770cf),
+    error: Color(0xffcf6679),
+  ),
+  surfaceMode: FlexSurfaceMode.levelSurfacesLowScaffold,
+  blendLevel: 13,
+  visualDensity: FlexColorScheme.comfortablePlatformDensity,
+  useMaterial3: true,
+  swapLegacyOnMaterial3: true,
+  // To use the Playground font, add GoogleFonts package and uncomment
+  // fontFamily: GoogleFonts.notoSans().fontFamily,
+);
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -15,75 +53,11 @@ class MyApp extends StatelessWidget {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Firebase demo',
-      // Made for FlexColorScheme version 7.0.5. Make sure you
-// use same or higher package version, but still same major version.
-// If you use a lower version, some properties may not be supported.
-// In that case remove them after copying this theme to your app.
-      theme: FlexThemeData.light(
-        colors: const FlexSchemeColor(
-          primary: Color(0xfffc6411),
-          primaryContainer: Color(0xfffeaa68),
-          secondary: Color(0xff2979ff),
-          secondaryContainer: Color(0xffe4eaff),
-          tertiary: Color(0xff2962ff),
-          tertiaryContainer: Color(0xffcbd6ff),
-          appBarColor: Color(0xffe4eaff),
-          error: Color(0xffb00020),
-        ),
-        surfaceMode: FlexSurfaceMode.levelSurfacesLowScaffold,
-        blendLevel: 7,
-        appBarStyle: FlexAppBarStyle.primary,
-        visualDensity: FlexColorScheme.comfortablePlatformDensity,
-        useMaterial3: true,
-        swapLegacyOnMaterial3: true,
-        // To use the playground font, add GoogleFonts package and uncomment
-        // fontFamily: GoogleFonts.notoSans().fontFamily,
-      ),
-      darkTheme: FlexThemeData.dark(
-        colors: const FlexSchemeColor(
-          primary: Color(0xffffb300),
-          primaryContainer: Color(0xff00325b),
-          secondary: Color(0xff82b1ff),
-          secondaryContainer: Color(0xff3770cf),
-          tertiary: Color(0xff448aff),
-          tertiaryContainer: Color(0xff0b429c),
-          appBarColor: Color(0xff3770cf),
-          error: Color(0xffcf6679),
-        ),
-        surfaceMode: FlexSurfaceMode.levelSurfacesLowScaffold,
-        blendLevel: 13,
-        visualDensity: FlexColorScheme.comfortablePlatformDensity,
-        useMaterial3: true,
-        swapLegacyOnMaterial3: true,
-        // To use the Playground font, add GoogleFonts package and uncomment
-        // fontFamily: GoogleFonts.notoSans().fontFamily,
-      ),
-// If you do not have a themeMode switch, uncomment this line
-// to let the device system mode control the theme mode:
-// themeMode: ThemeMode.system,
-
+      theme: lightTheme,
+      darkTheme: darkTheme,
+      themeMode: ThemeMode.system,
       initialRoute: '/',
-      getPages: [
-        GetPage(
-          name: '/',
-          page: () => LoginPage(),
-        ),
-        // GetPage(name: '/', page: () => const HomePage()),
-        GetPage(
-          name: '/login',
-          page: () => LoginPage(),
-        ),
-        GetPage(
-          name: '/register',
-          page: () => SignUpPage(),
-        ),
-        GetPage(name: '/home', page: () => const HomePage(), middlewares: [
-          ProtectedRoute(),
-        ]),
-        // GetPage(name: '/profile', page: () => const ProfilePage()),
-        // GetPage(name: '/settings', page: () => const SettingsPage()),
-        // GetPage(name: '/about', page: () => const AboutPage()),
-      ],
+      getPages: appPages,
     );
   }
 }
