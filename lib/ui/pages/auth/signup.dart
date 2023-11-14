82ba6675e8107ca801ef7e6cd78eab4bf59dce78
LeoLizc <ldlizcano@uninorte.edu.co>
2023-11-14 16:30:56 -0500

    Remove unused imports from auth and home pages

--- lib/ui/pages/auth/signup.dart
+++ lib/ui/pages/auth/signup.dart
@@ -1,4 +1,3 @@
-import 'package:f_parche/navigation.dart';
 import 'package:f_parche/ui/controllers/auth_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
