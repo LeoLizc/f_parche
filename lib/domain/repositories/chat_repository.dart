56b1307e1edf108194e25ee15e1b6a3cfc1ae751
LeoLizc <ldlizcano@uninorte.edu.co>
2023-11-20 00:16:26 -0500

    Update getChat method return type

--- lib/domain/repositories/chat_repository.dart
+++ lib/domain/repositories/chat_repository.dart
@@ -1,6 +1,6 @@
 import 'package:f_parche/domain/entities/chat.dart';
 
 abstract class ChatRepository {
-  Future<Chat> getChat(String id);
+  Future<Chat?> getChat(String id);
   Future<Chat> addChat(Chat chat);
 }
